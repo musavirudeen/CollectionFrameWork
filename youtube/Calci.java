8ec14882afc113075da387ab21d088fdd5f1c253
musavirudeen <musavirudeen@gmail.com>
2024-01-03 22:02:04 +0530

    File added from local

---
+++ youtube/Calci.java
@@ -0,0 +1,43 @@
+package calci.from.youtube;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Calci {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Calci window = new Calci();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Calci() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
